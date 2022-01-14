de577e62153fb48e34e242b7961a3684b8fb86d0
marcusr-cd <86986386+marcusr-cd@users.noreply.github.com>
2022-01-14 19:30:46 -0300

    Feature/enerpro bill (#1)
    * Add enerpro sample and mapped on json
    * Remove unnecessary import

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -1,3 +1,4 @@
+from ssl import ALERT_DESCRIPTION_UNSUPPORTED_EXTENSION
 import numpy as np
 import cv2
 from pdf2image import convert_from_bytes
@@ -11,6 +12,34 @@ def convert2Image(file, filename):
     page.save(buffer, format="jpeg")
     img = buffer.getbuffer()
   return cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_UNCHANGED)
+
+# def transform_image(image):
+#   original = image.copy()
+#   (H, W) = image.shape[:2]
+#   gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#   blur = cv2.GaussianBlur(gray, (7, 7), 0)
+#   edged = cv2.Canny(blur, 60, 160)
+#   contours = find_contours(edged.copy())
+
+#   for c in contours:
+#     peri = cv2.arcLength(c, True)
+#     aprox = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+#     if len(aprox) == 4:
+#       maior = aprox
+#       break
+#   cv2.drawContours(image, maior, -1, (120, 255, 0), 28)
+#   cv2.drawContours(image, [maior], -1, (120, 255, 0), 2)
+#   pontonsMaior = ordenar_pontos(maior)
+#   pts1 = np.float32(pontosMaior)
+#   pts2 = np.float32([[0,0], [W, 0], [W, H], [0, H]])
+
+#   matriz = cv2.getPerspectiveTransform(pts1, pts2)
+#   transform = cv2.warpPerspective(original, matriz, (W, H))
+
+#   return transform
+
+# def processamento_img(img)
 
 def preprocess(file, filename):
   img = convert2Image(file, filename)
